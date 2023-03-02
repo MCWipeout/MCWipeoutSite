8ac23b89e32b58667ec4cf58d8a42893c11374d5
Sorbetes <47263399+Sorbetess@users.noreply.github.com>
2023-03-02 13:29:35 +0800

    Added twitter embeds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import WipeoutHero from "$lib/components/WipeoutHero.svelte";
+	import WipeoutHero from '$lib/components/WipeoutHero.svelte';
+
 	let imageArray: number[] = [];
 	for (let i = 0; i < 17; i++) {
 		imageArray.push(i);
 	}
+
+	let screenshotHeroHeight: number;
 </script>
 
 <svelte:head>
 	<title>MC Wipeout</title>
-	<meta
-		name="description"
-		content="MC Wipeout - Minecraft parkour, but with a twist!"
-	/>
+	<meta name="description" content="MC Wipeout - Minecraft parkour, but with a twist!" />
 	<link rel="icon" type="image/png" href="/logo.png" />
 </svelte:head>
 
@@ -37,71 +37,51 @@
 	</div>
 </div>
 <div class="hero py-16 bg-base-100">
-	<div class="hero-content flex-col w-full">
-		<h1 class="text-5xl font-bold grow mb-4">Screenshots</h1>
-		<div class="carousel rounded-box w-full">
-			{#each imageArray as img}
-				<div id="slide{img + 1}" class="carousel-item relative w-full">
-					<img
-						src="/background-img/presskit-{img + 1}.png"
-						class="w-full"
-						alt="Screenshot {img + 1}"
-					/>
+	<div class="hero-content flex flex-col items-stretch lg:flex-row w-full">
+		<div class="flex flex-col lg:basis-2/3 flex-wrap">
+			<h1 class="text-5xl font-bold grow mb-4 text-center">Screenshots</h1>
+			<div class="carousel rounded-box">
+				{#each imageArray as img}
 					<div
-						class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+						id="slide{img + 1}"
+						class="carousel-item relative w-full"
+						bind:clientHeight={screenshotHeroHeight}
 					>
-						<a
-							href="#slide{((((img - 1) % imageArray.length) + imageArray.length) %
-								imageArray.length) +
-								1}"
-							class="btn btn-circle">❮</a
+						<img
+							src="/background-img/presskit-{img + 1}.png"
+							class="w-full"
+							alt="Screenshot {img + 1}"
+						/>
+						<div
+							class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
 						>
-						<a
-							href="#slide{((((img + 1) % imageArray.length) + imageArray.length) %
-								imageArray.length) +
-								1}"
-							class="btn btn-circle">❯</a
-						>
+							<a
+								href="#slide{((((img - 1) % imageArray.length) + imageArray.length) %
+									imageArray.length) +
+									1}"
+								class="btn btn-circle">❮</a
+							>
+							<a
+								href="#slide{((((img + 1) % imageArray.length) + imageArray.length) %
+									imageArray.length) +
+									1}"
+								class="btn btn-circle">❯</a
+							>
+						</div>
 					</div>
-				</div>
-				<!-- 
-				<div id="slide1" class="carousel-item relative w-full">
-					<img src="/background-img/presskit-1.png" class="w-full" />
-					<div
-						class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-					>
-						<a href="#slide4" class="btn btn-circle">❮</a>
-						<a href="#slide2" class="btn btn-circle">❯</a>
-					</div>
-				</div>
-				<div id="slide2" class="carousel-item relative w-full">
-					<img src="/background-img/presskit-2.png" class="w-full" />
-					<div
-						class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-					>
-						<a href="#slide1" class="btn btn-circle">❮</a>
-						<a href="#slide3" class="btn btn-circle">❯</a>
-					</div>
-				</div>
-				<div id="slide3" class="carousel-item relative w-full">
-					<img src="/background-img/presskit-3.png" class="w-full" />
-					<div
-						class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-					>
-						<a href="#slide2" class="btn btn-circle">❮</a>
-						<a href="#slide4" class="btn btn-circle">❯</a>
-					</div>
-				</div>
-				<div id="slide4" class="carousel-item relative w-full">
-					<img src="/background-img/presskit-4.png" class="w-full" />
-					<div
-						class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
-					>
-						<a href="#slide3" class="btn btn-circle">❮</a>
-						<a href="#slide1" class="btn btn-circle">❯</a>
-					</div>
-				</div> -->
-			{/each}
+				{/each}
+			</div>
+		</div>
+		<div class="lg:basis-1/3 w-full h-full flex flex-col">
+			<h1 class="text-5xl font-bold grow mb-4 text-center">Tweets</h1>
+			<div class="overflow-y-scroll rounded-xl">
+				<a
+					class="twitter-timeline"
+					data-height={screenshotHeroHeight}
+					data-theme="dark"
+					href="https://twitter.com/mc_wipeout?ref_src=twsrc%5Etfw">Tweets by MC Wipeout</a
+				> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
 		</div>
 	</div>
 </div>
