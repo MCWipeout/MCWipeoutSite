1fd6f8445c3eb5b92e088596226f5c09cebc119b
Sorbetes <scsl579@gmail.com>
2023-10-15 23:19:51 +0800

    Prep for season 3 player list

--- src/routes/seasons/Template.svelte
+++ src/routes/seasons/Template.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
 	import Carousel from '$lib/components/Carousel.svelte';
-    
-    export let eventName: string;
-    export let images: string[];
+
+	export let eventName: string;
+	export let images: string[] = [];
 </script>
 
 <div class="hero pt-16 bg-base-100">
 	<div class="hero-content flex-col">
 		<div>
-			<h1 class="text-6xl font-bold text-center">{eventName}</h1> 
+			<h1 class="text-6xl font-bold text-center">{eventName}</h1>
 		</div>
 	</div>
 </div>
-<div class="hero pt-16 bg-base-200">
-	<div class="hero-content flex-col w-full items-start">
-		<slot name="description" />
-	</div>
-</div>
-<div class="hero py-16 bg-base-100">
-	<div class="hero-content flex flex-col items-stretch lg:flex-row w-full">
-		<div class="flex flex-col flex-wrap">
-			<h1 class="text-5xl font-bold grow mb-4 text-center">Screenshots</h1>
-			<Carousel {images}/>
+{#if $$slots.description}
+	<div class="hero pt-16 bg-base-200">
+		<div class="hero-content flex-col w-full items-start">
+			<slot name="description" />
 		</div>
 	</div>
-</div>
+{/if}
+{#if images.length}
+	<div class="hero py-16 bg-base-100">
+		<div class="hero-content flex flex-col items-stretch lg:flex-row w-full">
+			<div class="flex flex-col flex-wrap">
+				<h1 class="text-5xl font-bold grow mb-4 text-center">Screenshots</h1>
+				<Carousel {images} />
+			</div>
+		</div>
+	</div>
+{/if}
 <div class="hero bg-base-200">
 	<div class="hero-content flex flex-wrap w-full items-stretch">
-        <h1 class="text-5xl font-bold basis-full text-center">Leaderboards</h1>
+		<h1 class="text-5xl font-bold basis-full text-center">Leaderboards</h1>
 		<slot />
 	</div>
 </div>
--- src/routes/seasons/season-3/+page.svelte
+++ src/routes/seasons/season-3/+page.svelte
@@ -11,91 +11,81 @@
 	export let data: PageData;
 
 	const { teamList: PlayerData } = data;
-
-	let randomSample = PlayerData
-		.map(value => ({ value, sort: Math.random() }))
-		.sort((a, b) => a.sort - b.sort)
-		.map(({ value }) => value)
-		.slice(0, 10);
-
-	const finalists = randomSample;
 	const participants = PlayerData;
 
-	const sessions = Array.from(new Set(participants.map((t) => t.Session))).filter(s => s).sort(); 
+	const sessions = Array.from(new Set(participants.map((t) => t.Session)))
+		.filter((s) => s)
+		.sort();
 	let activeTab = sessions[0];
 	$: sessionParticipantList = participants.filter((t) => t.Session == activeTab);
 
 	let imageArray: string[] = [];
-	for (let i = 0; i < EventData.galleryCount; i++) {
-		imageArray.push(`/background-img/season-2/${i + 1}.webp`);
-	}
+	// for (let i = 0; i < EventData.galleryCount; i++) {
+	// 	imageArray.push(`/background-img/season-2/${i + 1}.webp`);
+	// }
 
 	function defaultTime() {
-		return "XX:XX";
+		return 'XX:XX';
 	}
 </script>
 
-<Template
-	eventName={EventData.eventName}
-	images={imageArray}
->
+<Template eventName={EventData.eventName}>
 	<div slot="description" class="prose">
 		<Description />
 	</div>
-	<h2 class="text-3xl font-bold basis-full">Finalists</h2>
+	<!-- <h2 class="text-3xl font-bold basis-full">Finalists</h2>
 	<div class="w-full grid lg:grid-cols-2 gap-4">
 		{#each finalists as team, i}
 			<TeamCard
-				teamName={team["Team Name"]}
+				teamName={team['Team Name']}
 				placement={i}
 				teamColor={team.Colour}
 				players={[
-					{ name: team["P1 Name"]?.trim() || team["P1"], href: team["P1 Twitter"] },
-					{ name: team["P2 Name"]?.trim() || team["P2"], href: team["P2 Twitter"] },
-					{ name: team["P3 Name"]?.trim() || team["P3"], href: team["P3 Twitter"] },
+					{ name: team['P1 Name']?.trim() || team['P1'], href: team['P1 Twitter'] },
+					{ name: team['P2 Name']?.trim() || team['P2'], href: team['P2 Twitter'] },
+					{ name: team['P3 Name']?.trim() || team['P3'], href: team['P3 Twitter'] }
 				]}
 				badges={[
 					// team.Session ? { name: team.Session, color: "red" } : undefined,
-					{ name: `Map 1: ${ team["Map 1 Time"]?.trim() || defaultTime() }` },
-					{ name: `Map 2: ${ team["Map 2 Time"]?.trim() || defaultTime() }` },
-					{ name: `Map 3: ${ team["Map 3 Time"]?.trim() || defaultTime() }` }
+					{ name: `Map 1: ${team['Map 1 Time']?.trim() || defaultTime()}` },
+					{ name: `Map 2: ${team['Map 2 Time']?.trim() || defaultTime()}` },
+					{ name: `Map 3: ${team['Map 3 Time']?.trim() || defaultTime()}` }
 				]}
-				largeDisplay={ team["Map 4 Time"]?.trim() || defaultTime() }
+				largeDisplay={team['Map 4 Time']?.trim() || defaultTime()}
 			/>
 		{/each}
 	</div>
-	<div class="divider basis-full" />
+	<div class="divider basis-full" /> -->
 	<h2 class="text-3xl font-bold basis-full">Participants</h2>
 	<div class="tabs tabs-boxed">
 		{#each sessions as session}
-			<div 
-				class="tab" 
+			<div
+				class="tab"
 				class:tab-active={activeTab == session}
-				style="{activeTab == session ? 'color: hsl(var(--n) / var(--tw-text-opacity))': ''}"
-				on:click={() => {activeTab = session}}
-				on:keypress={() => {activeTab = session}}
+				style={activeTab == session ? 'color: hsl(var(--n) / var(--tw-text-opacity))' : ''}
+				on:click={() => {
+					activeTab = session;
+				}}
+				on:keypress={() => {
+					activeTab = session;
+				}}
 			>
-				{ session }
+				{session}
 			</div>
 		{/each}
 	</div>
 	<div class="w-full min-h-screen">
 		<div class="w-full grid lg:grid-cols-2 gap-4">
 			{#each sessionParticipantList as team}
-			<TeamCard
-				teamName={team["Team Name"]}
-				teamColor={team.Colour}
-				players={[
-					{ name: team["P1 Name"]?.trim() || team["P1"], href: team["P1 Twitter"] },
-					{ name: team["P2 Name"]?.trim() || team["P2"], href: team["P2 Twitter"] },
-					{ name: team["P3 Name"]?.trim() || team["P3"], href: team["P3 Twitter"] },
-				]}
-				badges={[
-					{ name: `Map 1: ${ team["Map 1 Time"].trim() || defaultTime() }` },
-					{ name: `Map 2: ${ team["Map 2 Time"].trim() || defaultTime() }` },
-					{ name: `Map 3: ${ team["Map 3 Time"].trim() || defaultTime() }` }
-				]}
-			/>
+				<TeamCard
+					teamName={team['Team Name']}
+					teamColor={team.Colour}
+					players={[
+						{ name: team['P1 Name']?.trim() || team['P1'], href: team['P1 Twitter'] },
+						{ name: team['P2 Name']?.trim() || team['P2'], href: team['P2 Twitter'] },
+						{ name: team['P3 Name']?.trim() || team['P3'], href: team['P3 Twitter'] }
+					]}
+				/>
 			{/each}
 		</div>
 	</div>
