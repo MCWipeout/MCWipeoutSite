ca6c704e3350ed35ec6e9f3b86d27785d01b5948
Sorbetes <scsl579@gmail.com>
2023-10-15 23:36:24 +0800

    Initial layout for sponsor activity

---
+++ src/routes/voicemod/+page.svelte
@@ -0,0 +1,90 @@
+<div class="hero min-h-screen" style="background-image: url('/background-img/main/1.webp');">
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="hero-content text-center text-neutral-content">
+		<div>
+			<div class="flex flex-col lg:flex-row max-w-3xl items-center gap-8">
+				<img src="logo.webp" class="max-h-36" />
+				<svg
+					fill="#ffffff"
+					height="50px"
+					width="50px"
+					version="1.1"
+					id="Capa_1"
+					xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink"
+					viewBox="0 0 460.775 460.775"
+					xml:space="preserve"
+					><g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+						id="SVGRepo_tracerCarrier"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/><g id="SVGRepo_iconCarrier">
+						<path
+							d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
+						/>
+					</g></svg
+				>
+				<img src="/voicemod-contest/voicemod-logo.png" class="max-h-36" />
+			</div>
+			<h1 class="mb-5 mt-8 text-5xl font-bold max-w-md mx-auto">
+				MCWipeout fans and creators can win up to *insert money here*!
+			</h1>
+			<p class="mb-5 max-w-md mx-auto">
+				Submit a 30-second clip of you using VoiceMod to enter our raffle.
+			</p>
+			<button class="btn btn-primary">Submit a Clip</button>
+		</div>
+	</div>
+</div>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content">
+		<div class="max-w-prose prose">
+			<h1 class="font-bold">Terms and Conditions</h1>
+			<p>
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<ol>
+				<li>
+					Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a,
+					ultricies in, diam. Sed arcu. Cras consequat
+				</li>
+				<li>
+					Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna
+					eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+					facilisis luctus, metus.
+				</li>
+				<li>
+					Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate
+					sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.
+				</li>
+				<li>
+					Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut,
+					elementum vulputate, nunc.
+				</li>
+				<li>
+					Sed adipiscing ornare risus. Morbi est est, blandit sit amet, sagittis vel, euismod vel,
+					velit. Pellentesque egestas sem. Suspendisse commodo ullamcorper magna.
+				</li>
+				<li>
+					Nulla sed leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+					inceptos himenaeos.
+				</li>
+				<li>Fusce lacinia arcu et nulla. Nulla vitae mauris non felis mollis faucibus.</li>
+				<li>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat
+					mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper
+					suscipit, posuere a, pede.
+				</li>
+				<li>
+					Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.
+					Aenean dignissim pellentesque felis.
+				</li>
+				<li>
+					Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a,
+					ultricies in, diam. Sed arcu. Cras consequat.
+				</li>
+			</ol>
+		</div>
+	</div>
+</div>
